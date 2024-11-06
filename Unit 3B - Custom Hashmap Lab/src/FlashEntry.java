98636660a73aa03bddcf6db4f87032b6bc20e3e8
Aidan Kametz <73295047+SilverAidan@users.noreply.github.com>
2024-11-06 10:00:25 -0500

    new naming structure WITH SPACES!!!!

---
+++ Unit 3B - Custom Hashmap Lab/src/FlashEntry.java
@@ -0,0 +1,9 @@
+public class FlashEntry{
+    Streamer key;
+    WatchTime value;
+    
+    public SweetEntry(Streamer key, WatchTime value) {
+        this.key = key;
+        this.value = value;
+    }
+}

7225593bc02f56b17c25bae29d7fdddaf684366b
Aidan Kametz <73295047+SilverAidan@users.noreply.github.com>
2024-11-20 10:33:33 -0500

    sorter i hardly know her

--- Unit 4A - Sorts II/src/App.java
+++ Unit 4A - Sorts II/src/App.java
@@ -1,5 +1,50 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Node head = new Node(-99999);
+        for(int i = 0; i < 10; i++){
+            head.add((int)(Math.random() * 10));
+        }
+        System.out.println(head);
+        mergeLinkSort(head);
+        System.out.println(head);
+    }
+    public static Node mergeLinkSort(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node middle = findMiddle(head, head);
+        System.out.println(middle);
+        Node secondHalf = middle.next;
+        middle.next = null;
+        Node left = mergeLinkSort(head); //first half
+        Node right = mergeLinkSort(secondHalf); //second half
+        return merge(left, right);
+    }
+
+    public static Node merge(Node left, Node right) {
+        Node dummy = new Node(999); //dummy node
+        Node runner = dummy;
+        while(left != null && right != null) {
+            if((Integer)left.data < (Integer)right.data) {
+                runner.next = left;
+                left = left.next;
+            }else{
+                runner.next = right;
+                right = right.next;
+            }
+            runner = runner.next;
+        }
+        if(left == null)
+            runner.next = right;
+        else
+            runner.next = left;
+        return dummy.next;
+    }
+            
+    private static Node findMiddle(Node fast, Node slow) {
+        if(fast.next == null ||fast.next.next == null) {
+            return slow;
+        }
+        return findMiddle(fast.next.next, slow.next);
     }
 }
--- Unit 4A - Sorts II/src/Node.java
+++ Unit 4A - Sorts II/src/Node.java
@@ -1,3 +1,20 @@
 public class Node {
-
+    Object data;
+    Node next;
+    public Node(Object d){
+        data = d;
+    }
+    public void add(Object d){
+        if(this.next == null){
+            this.next = new Node(d);
+            return;
+        }
+        this.next.add(d);
+    }
+    public String toString(){
+        if(next == null){
+            return ""+data;
+        }
+        return data + "->" + this.next.toString();
+    }
 }

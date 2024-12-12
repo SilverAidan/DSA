dab1cb70915a27eda8b9d72a2da54178e6dd4d25
Aidan Kametz <73295047+SilverAidan@users.noreply.github.com>
2024-12-12 09:43:02 -0500

    COOL JFRAME

--- Unit 4B - Radix Sort Lab/src/RadixDriver.java
+++ Unit 4B - Radix Sort Lab/src/RadixDriver.java
@@ -1,12 +1,26 @@
-import java.awt.*;
+import java.io.File;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class RadixDriver {
 	public static void main(String[] args) throws InterruptedException {
 		JFrame frame = new JFrame();
 		frame.setSize(1500, 800);
-		frame.setTitle("Kirby Sorter");
+		frame.setTitle("Kirby Sorter 2.0");
 		frame.setLocation(225, 100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFileChooser j = new JFileChooser();
+		j.setCurrentDirectory(new File(".\\"));
+		while (true) {
+			j.showOpenDialog(null); 
+			File selectedFile = j.getSelectedFile();
+			if (selectedFile.getName().toLowerCase().endsWith(".csv")) {
+				break; 
+			}
+			JOptionPane.showMessageDialog(null, "Please choose a valid CSV file.", "Invalid File", JOptionPane.INFORMATION_MESSAGE);
+		}
+		File csv = j.getSelectedFile();
 		frame.setVisible(true);
 	}
 }

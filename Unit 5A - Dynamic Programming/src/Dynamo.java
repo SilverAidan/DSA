7624d93e1f9f64e89f4b1d0324b1ccb32340310a
Aidan Kametz <73295047+SilverAidan@users.noreply.github.com>
2024-12-20 10:39:33 -0500

    asd

--- Unit 5A - Dynamic Programming/src/Dynamo.java
+++ Unit 5A - Dynamic Programming/src/Dynamo.java
@@ -2,8 +2,11 @@ import java.util.HashMap;
 
 public class Dynamo {
     static HashMap<Integer, Long> memo = new HashMap<Integer, Long>();
+    static HashMap<Integer, Integer> totals = new HashMap<Integer, Integer>();
     public static void main(String[] args) throws Exception {
         System.out.println(fib(8));
+        int[] denoms = {1, 5, 10, 25};
+        System.out.println(cointCount(denoms, 34));
     }
 
     public static long fib(int n){
@@ -24,5 +27,26 @@ public class Dynamo {
         }
         System.out.println(n);
         return fibBad(n-1) + fibBad(n-2);
+    }
+
+    public static int cointCount(int[] denom, int total){
+        if(total < 0){
+           return Integer.MAX_VALUE;
+        }
+        if(total == 0){
+            return 0;
+        }
+        if(totals.containsKey(total)){
+            return totals.get(total);
+        }
+        int count = Integer.MAX_VALUE;
+        for(int coin: denom){
+            int result = cointCount(denom, total - coin);
+            if(result != Integer.MAX_VALUE) {
+                count = Math.min(count, result + 1);
+            }
+        }
+        totals.put(total, count);
+        return count;
     }
 }

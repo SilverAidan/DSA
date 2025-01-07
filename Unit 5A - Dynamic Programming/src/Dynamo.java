173d445bc4cf4a69c2b6300ec29ba55b9d2540fb
Aidan Kametz <73295047+SilverAidan@users.noreply.github.com>
2025-01-07 10:33:13 -0500

    french midterm

--- Unit 5A - Dynamic Programming/src/Dynamo.java
+++ Unit 5A - Dynamic Programming/src/Dynamo.java
@@ -3,61 +3,59 @@ import java.util.HashMap;
 public class Dynamo {
     static HashMap<Integer, Long> memo = new HashMap<Integer, Long>();
     static HashMap<Integer, Integer> totals = new HashMap<Integer, Integer>();
-    public static int MCount = 0;
-    public static int dumbDiceCount = 0;
+    public static int knapDumb = 0;
     public static void main(String[] args) throws Exception {
-        System.out.println(fibBad(8));
+        System.out.println(fib(8));
         int[] denoms = {1, 5, 10, 25};
         System.out.println(cointCount(denoms, 34));
-        System.out.println(diceCountDumb(8, 24));
-        System.out.println(dumbDiceCount);
-        System.out.println("Memo "+ diceCountMemo(8, 24));
-        System.out.println(MCount);
-
-        int[][] diceTable = new int[9][25];
-        for(int i = 0; i <=6; i++){
-            diceTable[1][i] = 1;
+        System.out.println(newton(4, 1000));
+        double[] values = {100, 200, 50, 300, 400, 25};
+        double[] weights = {3, 5, 10, 1, 2, 1};
+        System.out.println(knapsack(values, weights, 10, 0, 0));
+        System.out.println(knapDumb);
+    }
+    public static double knapSmart(double[] values, double[] weights, double capacity, int i, double max){
+        double[][] memo = new double[weights.length][(int)capacity+1];
+        for(int j = 0; j < weights.length; j++){
+            for(int k = 0; k < capacity+1; k++){
+                memo[j][k] = -1;
+            }
         }
+        return knapsackSmartH(values, weights, capacity, i, 0, memo)
+    }
+    public static double knapsackSmartH(double[] values, double[] weights, double capacity, int i, double max, double[][] memo){
+        knapDumb++;
+        if(i == values.length)
+            return max;
+        double with = 0;
+        if(weights[i] <= capacity)
+            with = knapsack(values, weights, capacity-weights[i], i+1, max+values[i]);
+        double without = knapsack(values, weights, capacity, i+1, max);
+        return Math.max(with, without);
     }
 
-    public static int diceCountDumb(int dice, int target){
-        dumbDiceCount++;
-        if(dice == 0 && target == 0){
+    public static double knapsack(double[] values, double[] weights, double capacity, int i, double max){
+        knapDumb++;
+        if(i == values.length)
+            return max;
+        double with = 0;
+        if(weights[i] <= capacity)
+            with = knapsack(values, weights, capacity-weights[i], i+1, max+values[i]);
+        double without = knapsack(values, weights, capacity, i+1, max);
+        return Math.max(with, without);
+    }
+    
+
+    public static long fib(int n){
+        if (n<=2){
             return 1;
         }
-        if(dice<=0||target <= 0){
-            return 0;
+        if(memo.containsKey(n)){
+            return memo.get(n);
         }
-        int count = 0;
-        for(int i = 1; i <= 6; i++){
-            count += diceCountDumb(dice - 1, target - i);
-        }
-        return count;
-    }
-
-    public static int diceCountMemo(int dice, int target){
-        int[][] memo = new int[dice + 1][target + 1];
-        return diceCountMemoHelper(dice, target, memo);
-        
-    }
-
-    private static int diceCountMemoHelper(int dice, int target, int[][] memo) {
-        MCount++;
-        if(dice == 0 && target == 0){
-            return 1;
-        }
-        if(dice<=0||target <= 0){
-            return 0;
-        }
-        if(memo[dice][target] != 0){
-            return memo[dice][target];
-        }
-        int count = 0;
-        for(int i = 1; i <= 6; i++){
-            count += diceCountMemoHelper(dice-1, target-i, memo);
-        }
-        memo[dice][target] = count;
-        return count;
+        memo.put(n, fib(n-1) + fib(n-2));
+        System.out.println(n);
+        return memo.get(n);
     }
 
     public static long fibBad(int n){
@@ -67,6 +65,25 @@ public class Dynamo {
         System.out.println(n);
         return fibBad(n-1) + fibBad(n-2);
     }
+
+    public static double newton(double guess, int trials){
+        if(trials == 0){
+            return Double.POSITIVE_INFINITY;
+        }
+        if(Math.abs(function(guess)) < 0.0000001){
+            return guess;
+        }
+        return newton(guess - function(guess)/derivative(guess), trials - 1);
+    }
+
+    private static double function(double x) {
+        return x*x-4;
+    }
+
+    private static double derivative(double x) {
+        return 2*x;
+    }
+
 
     public static int cointCount(int[] denom, int total){
         if(total < 0){

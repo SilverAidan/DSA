386169abb63a18246871cdadf766264fe65ba503
Aidan Kametz <73295047+SilverAidan@users.noreply.github.com>
2025-01-29 10:33:10 -0500

    UTS OVER!!!!!!

--- Unit 6A - Trees/src/Forrest.java
+++ Unit 6A - Trees/src/Forrest.java
@@ -1,5 +1,21 @@
 public class Forrest {
     public static void main(String[] args) {
         TNode oak = new TNode(20);
+        int[] arr = {15,17,14,30,21,1};
+        for(int a : arr){
+            oak.addNode(new TNode(a));
+        }
+        System.out.println(oak.inOrder());
+        System.out.println(oak.preOrder());
+        System.out.println(oak.postOrder());
+        System.out.println(oak.getHeight());
+        System.out.println(oak.left.getHeight());
+        System.out.println(oak.left.right.getHeight());
+
+        System.out.println(oak.get(15).inOrder());
+        System.out.println(oak.size());
+        TNode ancestor = new TNode(-999);
+        ancestor.right = oak;
+        System.out.println(ancestor.getParent(21).value);
     }
 }
--- Unit 6A - Trees/src/TNode.java
+++ Unit 6A - Trees/src/TNode.java
@@ -18,7 +18,7 @@ public class TNode {
             else{
                 this.left.addNode(tn);
             }
-        }else if(tn.value > this.value()){
+        }else if(tn.value > this.value){
             if(this.right == null){
                 this.right = tn;
             }
@@ -30,16 +30,78 @@ public class TNode {
 
     public String inOrder(){
         String output = "";
-        if(left == null){
-            output += this.value;
-        }else{
-            left.inOrder();
+        if(left != null){
+            output += left.inOrder();
         }
-
+        output += this.value +":";
         if(right==null){
             return output;
         }else{
             return output + right.inOrder();
         }
+    }
+
+    public String preOrder(){
+        String output = this.value +":";
+        if(left != null){
+            output += left.preOrder();
+        }
+        if(right==null){
+            return output;
+        }else{
+            return output + right.preOrder();
+        }
+    }
+
+    public String postOrder(){
+        String output = "";
+        if(left != null){
+            output += left.postOrder();
+        }
+        if(right!=null){
+            output += right.postOrder();
+        }
+        return output += this.value +":";
+    }
+
+    public int getHeight(){
+        return 1 + Math.max(left == null ? -1 : left.getHeight(), right == null ? -1 : right.getHeight());
+    }
+
+    public TNode get (Integer i){
+        if(value == i){
+            return this;
+        }
+        if(i<value){
+            return left == null ? null : left.get(i);
+        }
+        return right == null ? null : right.get(i);
+    }
+
+    public int size(){
+        return ((left == null ? 0 : left.size()) + (right == null ? 0 : right.size()) + 1);
+    }
+
+    public TNode delete(Integer key){
+        TNode parent = this.getParent(key);
+        if(parent == null){
+            return null;
+        }
+        if(parent.left == null){
+            parent.value = 
+        }
+    }
+
+    public TNode getParent(Integer key) {
+        if(left!=null && left.value == key){
+            return this;
+        }
+        if(right!=null && right.value == key){
+            return this;
+        }
+        if(key<value){
+            return left ==null?null:left.getParent(key);
+        }
+        return right == null?null:right.getParent(key);
     }
 }

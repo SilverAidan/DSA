db8dced9ad81865832aefa0ece78886afb5081e2
Aidan Kametz <73295047+SilverAidan@users.noreply.github.com>
2025-02-26 10:23:12 -0500

    new unit

---
+++ Unit 7A - Priority Queues & Heaps/src/Injury.java
@@ -0,0 +1,20 @@
+public class Injury implements Comparable<Injury>{
+    String ailment;
+    int priority;
+
+    public Injury(String ailment, int priority) {
+        super();
+        this.ailment = ailment;
+        this.priority = priority;
+    }
+
+    public Injury() {
+        ailment = "Fever";
+        priority = 5;
+    }
+
+    @Override
+    public int compareTo(Injury o) {
+        return this.priority-o.priority;
+    }
+}
---
+++ Unit 7A - Priority Queues & Heaps/src/Triage.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Triage {
+    ArrayList<Injury> injuries;
+
+    public Triage(ArrayList<Injury> inj){
+        injuries = inj;
+    }
+
+    public Injury findMin(){
+        return injuries.isEmpty() ? injuries.get(0) : null;
+    }
+
+    public Injury getLeft(int i){
+        int spot = 2*i+1;
+        return spot<injuries.size() ? injuries.get(spot) : null;
+    }
+
+    public Injury getLeft(Injury inj){
+        int spot = injuries.indexOf(inj);
+        return spot > -1 ? this.getLeft(spot) : null;
+    }
+
+    public Injury getRight(int i){
+        int spot = i+2;
+        return spot<injuries.size() ? injuries.get(spot) : null;
+    }
+
+    public Injury getRight(Injury inj){
+        int spot = injuries.indexOf(inj);
+        return spot > -1 ? this.getRight(spot) : null;
+    }
+
+    
+}
